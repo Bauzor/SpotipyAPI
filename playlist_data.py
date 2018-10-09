4879b837657535b11f9bbd48494951c47a18dbed
bauzor <bjgau@outlook.com>
2018-10-09 01:54:48 -0400

    lmao

--- playlist_data.py
+++ playlist_data.py
@@ -6,31 +6,29 @@ import csv
 with open('playlists.csv', 'w', newline='') as csvfile:
     fieldnames = [] #use spotify api to find a list of the fieldnames for playlists
 
-if __name__ == '__main__':
-    if len(sys.argv) > 3:
-        username = sys.argv[1]
-        client_id = sys.argv[2]
-        client_secret = sys.argv[3]
-    else:
-        print("usage: python local_files.py [username] [client_id] [client_secret]")        
-        sys.exit()
-        
-token = util.prompt_for_user_token(
-    username=username,
-    scope='playlist-read-private',
-    client_id=client_id,
-    client_secret=client_secret,
-    redirect_uri='https://example.com/callback/'
-)
-
-if token:
-    sp = spotipy.Spotify(auth=token)
-    playlists = sp.user_playlists(username)
-    for key in playlists:
-        if key == 'items':
-            print(key)
+    if __name__ == '__main__':
+        if len(sys.argv) > 3:
+            username = sys.argv[1]
+            client_id = sys.argv[2]
+            client_secret = sys.argv[3]
         else:
-            print('not items')
+            print("usage: python local_files.py [username] [client_id] [client_secret]")        
+            sys.exit()
+            
+    token = util.prompt_for_user_token(
+        username=username,
+        scope='playlist-read-private',
+        client_id=client_id,
+        client_secret=client_secret,
+        redirect_uri='https://example.com/callback/'
+    )
 
-else:
-    print("Can't get token for %s" %username)
+    if token:
+        sp = spotipy.Spotify(auth=token)
+        paging = sp.user_playlists(username)
+        for playlist_key in paging['items'][0]:
+            fieldnames.append(playlist_key)
+        for playlist in paging['items']:
+            
+    else:
+        print("Can't get token for %s" %username)
